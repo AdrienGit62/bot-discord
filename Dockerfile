bfe842b28a807f0c641be5d2ea5bcef1123e76cf
FORLASV <93997804+FORLASV@users.noreply.github.com>
2022-01-24 08:49:46 +0100

    jkn

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3
 RUN mkdir -p /usr/src/bot
 WORKDIR /usr/src/bot
 RUN pip install discord
+RUN pip install git+https://github.com/Rapptz/discord.py.git@master
 
 COPY . .
 
--- my_discord_bot.py
+++ my_discord_bot.py
@@ -9,6 +9,8 @@ if TOKEN == '' :
 
 client = discord.Client()
 
+prefix = "!"
+
 @client.event
 async def on_ready():
     print('We have successfully loggged in as {0.user}'.format(client))
@@ -19,16 +21,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower() == 'ping':
-        await message.channel.send(f"__***----------------Je t'ai à l'oeil, {message.author.display_name}! 👀 ------------------***__")
-        embed = discord.Embed(title="Forlas bot vous aide !!", description="Apprenez à utiliser discord", color=0xeee657)
-
-        embed.add_field(name="Fermer Discord", value="ALT+F4 pour fermer discord ou sinon vous avez une croix en haut à droite", inline=False)
-        embed.add_field(name="Ne pas parler", value="Si tu la ferme ça vaut mieux pour toi x)", inline=False)
-        embed.add_field(name="Si vous avez un mac :", value="Acheter un windows !!", inline=False)
-        embed.add_field(name="Les gens qui utilise H24 linux", value="Sachez que je vous soutiens pas", inline=False)
-
+    if message.content.lower() == '{prefix}help':
+        embed = discord.Embed(title="__***APPCLS vous donne les différentes commandes disponibles !!***__", description="", color=0xeee657)
+        embed.add_field(name="Ajouter un devoir:", value="Tapez la commande : !add [titre_du_devoir]", inline=False)
+        view = discord.ui.View()
+        item = discord.ui.Button(style=discord.ButtonStyle.blurple, label="Click Me", url="https://google.com")
+        view.add_item(item=item)
+        await message.channel.send("This message has a button!", view=view)
         await message.channel.send(embed=embed)
         return
+
+
 
 client.run(TOKEN)
